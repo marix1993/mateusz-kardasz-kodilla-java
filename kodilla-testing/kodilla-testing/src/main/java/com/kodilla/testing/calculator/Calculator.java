7d0b37276b6f812754878d8025cc0ecae2726e71
marix1993 <105885181+marix1993@users.noreply.github.com>
2022-05-24 20:06:24 +0100

    6.2 Added kodilla-testing module

---
+++ kodilla-testing/kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public double addAToB(double a, double b) {
+        return a + b;
+    }
+    public double subtractAFromB(double a, double b) {
+        return a - b;
+    }
+}
