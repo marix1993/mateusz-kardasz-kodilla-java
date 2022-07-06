23cf6f2d0930fff568e3398cd7d29ca6e303eb6e
marix1993 <105885181+marix1993@users.noreply.github.com>
2022-07-06 21:20:58 +0100

    7.1 Added kodilla-stream

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,37 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String textFirst = poemBeautifier.beautify("Text to beautify ", (text -> "ABC" + text + "ABC"));
+        System.out.println(textFirst);
+
+        String textSecond = poemBeautifier.beautify("Text to beautify ", (text -> text.toUpperCase()));
+        System.out.println(textSecond);
+
+        String textThird = poemBeautifier.beautify("Text to beautify ", (text -> text.replace(" ", "***")));
+        System.out.println(textThird);
+
+        String textFourth = poemBeautifier.beautify("Text to beautify ", (text -> text.repeat(4)));
+        System.out.println(textFourth);
+
+
+        //System.out.println("Calculating expressions with lambdas");
+        //expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        //expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        //expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        //expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        //System.out.println("Calculating expressions with method references");
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+    }
+}
