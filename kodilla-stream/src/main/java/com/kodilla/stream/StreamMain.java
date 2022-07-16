576530bc5ba3f3a70c2fc5d35f83ad035300f45b
marix1993 <105885181+marix1993@users.noreply.github.com>
2022-07-16 18:07:28 +0100

    7.2 kodilla-added

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,25 +1,26 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+        //ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String textFirst = poemBeautifier.beautify("Text to beautify ", (text -> "ABC" + text + "ABC"));
-        System.out.println(textFirst);
+        //PoemBeautifier poemBeautifier = new PoemBeautifier();
+        //String textFirst = poemBeautifier.beautify("Text to beautify ", (text -> "ABC" + text + "ABC"));
+        //System.out.println(textFirst);
 
-        String textSecond = poemBeautifier.beautify("Text to beautify ", (text -> text.toUpperCase()));
-        System.out.println(textSecond);
+        //String textSecond = poemBeautifier.beautify("Text to beautify ", (text -> text.toUpperCase()));
+        //System.out.println(textSecond);
 
-        String textThird = poemBeautifier.beautify("Text to beautify ", (text -> text.replace(" ", "***")));
-        System.out.println(textThird);
+        //String textThird = poemBeautifier.beautify("Text to beautify ", (text -> text.replace(" ", "***")));
+        //System.out.println(textThird);
 
-        String textFourth = poemBeautifier.beautify("Text to beautify ", (text -> text.repeat(4)));
-        System.out.println(textFourth);
+        //String textFourth = poemBeautifier.beautify("Text to beautify ", (text -> text.repeat(4)));
+        //System.out.println(textFourth);
 
 
         //System.out.println("Calculating expressions with lambdas");
